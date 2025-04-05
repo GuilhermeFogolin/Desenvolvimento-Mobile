0cbcca7ffc098f7b1fb33ce5ea6dd1dfc44fc9dd
GuilhermeFogolin <guilhermefogolin02@gmail.com>
2025-04-05 20:54:05 -0300

    Tela inicial e de cadastro

--- Tem-De-Tudo/app/src/main/java/br/com/fecapccp/tem_de_tudo/MainActivity.java
+++ Tem-De-Tudo/app/src/main/java/br/com/fecapccp/tem_de_tudo/MainActivity.java
@@ -1,6 +1,9 @@
 package br.com.fecapccp.tem_de_tudo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,70 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Instanciando
+
+    private Button btnMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Log.i("Ciclo de Vida", "Tela inicial - onCreate");
+
+        // Vinculando btn com view pelo ID:
+
+        btnMain = findViewById(R.id.btnMain);
+
+        // Chamando a próxima tela:
+
+        btnMain.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Cadastro.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Ciclo de Vida", "Tela inicial - onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Ciclo de Vida", "Tela inicial - onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Ciclo de Vida", "Tela inicial - onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Ciclo de Vida", "Tela inicial - onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Ciclo de Vida", "Tela inicial - onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Ciclo de Vida", "Tela inicial - onStart");
+    }
+
 }
