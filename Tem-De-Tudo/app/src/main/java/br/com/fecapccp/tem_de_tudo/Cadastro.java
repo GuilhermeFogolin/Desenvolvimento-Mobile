1f2db3cdc3463e4a014ea564ff75a21708c81137
GuilhermeFogolin <guilhermefogolin02@gmail.com>
2025-04-06 08:36:34 -0300

    Startando FinalActivity

--- Tem-De-Tudo/app/src/main/java/br/com/fecapccp/tem_de_tudo/Cadastro.java
+++ Tem-De-Tudo/app/src/main/java/br/com/fecapccp/tem_de_tudo/Cadastro.java
@@ -1,6 +1,10 @@
 package br.com.fecapccp.tem_de_tudo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,76 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Cadastro extends AppCompatActivity {
 
+    private Button btnCadastrar;
+    private EditText et_nome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cadastro);
+
+        Log.i("Ciclo de Vida", "Tela cadastro - onCreate");
+
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+        et_nome = findViewById(R.id.et_nome);
+
+        btnCadastrar.setOnClickListener(view -> {
+            cadastro();
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btnCadastro), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Ciclo de Vida", "Tela cadastro - onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Ciclo de Vida", "Tela cadastro - onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Ciclo de Vida", "Tela cadastro - onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Ciclo de Vida", "Tela cadastro - onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Ciclo de Vida", "Tela cadastro - onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Ciclo de Vida", "Tela cadastro - onStart");
+    }
+
+    private void cadastro() {
+
+        String nome = et_nome.getText().toString();
+
+        // Recuperando valores e inicializando a intent
+
+        Intent intent = new Intent(this, Final.class);
+        intent.putExtra("nome", nome);
+        startActivity(intent);
+
     }
 }
--- Tem-De-Tudo/app/src/main/java/br/com/fecapccp/tem_de_tudo/Final.java
+++ Tem-De-Tudo/app/src/main/java/br/com/fecapccp/tem_de_tudo/Final.java
@@ -1,6 +1,11 @@
 package br.com.fecapccp.tem_de_tudo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,77 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Final extends AppCompatActivity {
 
+    private TextView tv_final;
+    private Button btnFechar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_final);
+
+        Log.i("Ciclo de Vida", "Tela final - onCreate");
+
+        tv_final = findViewById(R.id.tv_final);
+        btnFechar = findViewById(R.id.btnFechar);
+
+        // Recebendo os dados da tela de cadastro: Intent e itens extras.
+
+        Bundle bundle = getIntent().getExtras();
+
+        String nome = bundle.getString("nome");
+
+        // Imprimindo o resultado:
+
+        tv_final.setText("Seja muito bem-vindo(a), " + nome + "!");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Ciclo de Vida", "Tela final - onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Ciclo de Vida", "Tela final - onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Ciclo de Vida", "Tela final - onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Ciclo de Vida", "Tela final - onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Ciclo de Vida", "Tela final - onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Ciclo de Vida", "Tela final - onStart");
+    }
+
+    // Função para retornar para tela principal:
+    public void fechar(View v) {
+        Intent intent = new Intent(Final.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
